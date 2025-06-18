bb890b74f85ee8d9580aa417c4b999fe47859886
KATHIA Hammami <hammamikathia@outlook.fr>
2025-06-17 22:01:34 -0400

    Composant CharacterDetailsComponent

--- src/app/components/character-details/character-details.component.ts
+++ src/app/components/character-details/character-details.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 
 @Component({
   selector: 'app-character-details',
-  imports: [],
-  templateUrl: './character-details.component.html',
-  styleUrl: './character-details.component.scss'
+  templateUrl: './character-details.component.html'
 })
 export class CharacterDetailsComponent {
+  id: number = 0;
+  character: any = null;
+  errorMessage: string = '';
+  loading = false;
 
+  constructor(private api: RickAndMortyService) {}
+
+  fetchCharacter() {
+    this.errorMessage = '';
+    this.character = null;
+
+    if (this.id <= 0 || !Number.isInteger(this.id)) {
+      this.errorMessage = 'Veuillez entrer un ID entier positif.';
+      return;
+    }
+
+    this.loading = true;
+    this.api.getCharacterById(this.id).subscribe({
+      next: data => {
+        this.character = data;
+        this.loading = false;
+      },
+      error: err => {
+        this.errorMessage = 'Personnage introuvable.';
+        this.loading = false;
+      }
+    });
+  }
 }
