60587244ffb7bc6a6fd253318e1bc8fade82ec0f
KATHIA Hammami <hammamikathia@outlook.fr>
2025-06-17 21:54:05 -0400

    rick-and-morty.service.ts

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickAndMortyService {
+  private baseUrl = 'https://rickandmortyapi.com/api/character';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCharacters(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  getCharacterById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
 }
